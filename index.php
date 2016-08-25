a765039f72a5525f010fd1133770dfe497e61187
Emil <emil88pl@gmail.com>
2016-08-25 14:46:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html>
+<head>
+<title>PHP</title>
+</head>
+
+<body>
+
+<form action="process.php" method="post">
+Enter name:<input type="text" name="name">
+<input type="submit">
+</form>
+
+</body>
+
+</html>
---
+++ process.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>PHP</title>
+</head>
+
+<body>
+
+<?php 
+
+    $name = $_POST["name"];
+    echo "Hello, " . $name;
+?>
+
+</body>
+
+
+
+</html>
